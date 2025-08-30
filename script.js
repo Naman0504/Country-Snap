c5e10c7843e4b61f131e277a344de20352a7cd92
Naman0504 <matiyaranaman@gmail.com>
2025-08-30 16:23:44 +0530

    add filter by region dropdown feature

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const contryContainer = document.querySelector(".country-container");
 const searchBox = document.querySelector(".search-box");
+const Select = document.querySelector(".filter-by-region");
 
 let allCountries = [];
+let regions;
 
 fetch(
   "https://restcountries.com/v3.1/all?fields=name,flags,capital,population,region"
@@ -10,7 +12,20 @@ fetch(
   .then((data) => {
     allCountries = data;
     showCountries(allCountries);
-    console.log(data);
+    // getDistinctRegion(allCountries);
+    console.log("R", regions);
+    regions = getDistinctRegion(allCountries);
+
+    Select.innerHTML = `<option hidden="">Filter by Region</option>`;
+
+    regions.forEach((region) => {
+      const option = document.createElement("option");
+      option.value = region;
+      option.textContent = region;
+      Select.appendChild(option);
+    });
+  
+    // showRegion()
   })
   .catch((error) => console.log("Error:", error));
 
@@ -49,5 +64,24 @@ searchBox.addEventListener("input", (e) => {
     country.name.common.toLowerCase().includes(query ? query : "")
   );
   // return e.target.value;
+  showCountries(filteredCounrties);
+});
+
+function getDistinctRegion(countries) {
+  const regions = countries.map((c) => c.region);
+  return [...new Set(regions)];
+  console.log("-------", regions);
+}
+
+
+
+
+Select.addEventListener("change", (e) => {
+  // console.log(e.target.value.toLowerCase());
+  const query = e.target.value.toLowerCase();
+
+  const filteredCounrties = allCountries.filter((country) =>
+    country.region.toLowerCase().includes(query)
+  );
   showCountries(filteredCounrties);
 });
